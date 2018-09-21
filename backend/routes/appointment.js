e0019bf053a2443028bb22cddf8421a761ac1f1a
Diego Hernando Mahecha Vargas <dh.mahecha@uniandes.edu.co>
2018-09-20 23:50:15 -0500

    Ajustes

--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -7,6 +7,5 @@ router.post('/', AppointmentController.setAppointment);
 //router.post('/notification/', AppointmentController.setNotificationAppointmentBy_id);
 router.get('/:_id', AppointmentController.getAppointmentBy_id);
 router.get('/confirm/:_id', AppointmentController.setAppointmentUpdate);
-//router.get();
 
 module.exports = router;
