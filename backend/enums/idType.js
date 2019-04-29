ca01f46ba3cda68d0ed4d96b750577fb912bfe04
Diego Hernando Mahecha Vargas <dh.mahecha@uniandes.edu.co>
2019-04-28 19:52:23 -0500

    se crea tipo de documento Pasaporte

--- backend/enums/idType.js
+++ backend/enums/idType.js
@@ -4,7 +4,8 @@ const IdType = Object.freeze({
     CEDULA: "Cédula",
     TARJETA_IDENTIDAD: "Tarjeta de Identidad",
     CEDULA_EXTRANJERIA: "Cédula de Extranjería",
-    REGISTRO_CIVIL: "Registro Civil"
+    REGISTRO_CIVIL: "Registro Civil",
+    PASAPORTE: "Pasaporte"
   });
 
 module.exports =  IdType;
